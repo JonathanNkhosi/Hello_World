95590b255823a849ff24f26e2afd36135635edc3
Jonathan <Jonathan37mumba@gmail.com>
2026-02-13 15:09:10 +0200

    created a calculator file

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
  public static void main(String[] args) {
-   System.out.print("Hello Jonathan");
-   System.out.print("You are going places!");
+   System.out.println("Hello Jonathan");
+   System.out.println("You are going places!");
  }
 }
