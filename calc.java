58b997be2fb6924b0825cfd1071870ee688bdb84
Jonathan <Jonathan37mumba@gmail.com>
2026-02-13 22:02:23 +0200

    Modified calulator to make it more interactive

--- calc.java
+++ calc.java
@@ -11,16 +11,28 @@ public class calc {
   System.out.print("Enter your second number: ");
   double num2 = input.nextDouble();
 
-  double sum = num1 + num2;
-  double difference = num1 - num2;
-  double product = num1 * num2;
-  double quotient = num1 / num2;
+  System.out.println("--Menue--");
+  System.out.println("1. Addition");
+  System.out.println("2. Subtraction");
+  System.out.println("3. Multiplication");
+  System.out.println("4. Division");
+  System.out.println("Pick your operation from the menue: ");
+  int choice = input.nextInt();
 
-  System.out.println("Sum: "+sum);
-  System.out.println("Difference: "+difference);
-  System.out.println("Product: "+product);
-  System.out.println("Quotient: "+quotient);
-
+ if(choice==1) {
+   double sum = num1 + num2;
+   System.out.print("Your sum is: " + sum);
+   } else if (choice==2) {
+       double difference = num1 - num2;
+       System.out.print("Your difference is: " + difference);
+   } else if (choice==3) {
+       double product = num1 * num2;
+       System.out.print("Your Product is: " + product);
+   } else if (choice==4) {
+       double quotient = num1/num2;
+       System.out.print("Your quotient is: " + quotient);
+   }
+     
   input.close();
  }
 }
